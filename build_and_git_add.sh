caec00dddaa936f5be4fecbfc4a112d6bb0f022f
Benedikt Kleinmeier <kleinmeier.benedikt@gmail.com>
2022-03-31 10:56:51 +0200

    Added Bash script "build_and_git_add.sh"

---
+++ build_and_git_add.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Switch to "main" branch, clean the directory from Jekyll artifacts,
+# build the site in the upper directory, switch to "site" branch and add build artifacts there.
+
+git checkout main
+git reset --hard HEAD
+bundle exec jekyll clean
+bundle exec jekyll build --destination ../_site
+git checkout site
+rm -fr *
+mv -t . ../_site/*
+git add -A .
