7a058227e7c92e57690255d13ccd4ce5c124cbbb
Benedikt Kleinmeier <kleinmeier.benedikt@gmail.com>
2022-04-16 13:41:46 +0200

    Use consistent colors in ".navbar" and added link to Vadere source code on LRZ GitLab

--- build_and_git_add.sh
+++ build_and_git_add.sh
@@ -5,10 +5,14 @@
 
 git checkout main
 git reset --hard HEAD
+
 bundle exec jekyll clean
 bundle exec jekyll build --destination ../_site
+
 git checkout site
 rm -fr *
 mv -t . ../_site/*
 git add -A .
 rm -r ../_site
+
+echo "Changes on \"main\": $(git show -s --format=%s)"
